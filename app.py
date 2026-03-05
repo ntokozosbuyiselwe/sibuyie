38165af33b9d85940238cdcc13f747908c2cdf74
Ntokozo Mbuyazi <ntokozosbuyiselwe@gmail.com>
2026-03-05 20:36:41 +0200

    Added SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app= Flask(__name__)
+
+# Configure SQLite database (for demo purposes)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
 
 
 
